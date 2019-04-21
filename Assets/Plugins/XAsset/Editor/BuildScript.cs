f01414186d2534d9c75f2a2a9964e75c2d00a6b9
fengjiyuan <583280516@qq.com>
2019-04-21 21:47:44 +0800

    工程资源导入

---
+++ Assets/Plugins/XAsset/Editor/BuildScript.cs
@@ -0,0 +1,345 @@
+//
+// BuildScript.cs
+//
+// Author:
+//       fjy <jiyuan.feng@live.com>
+//
+// Copyright (c) 2019 fjy
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using UnityEditor;
+using UnityEngine;
+
+namespace Plugins.XAsset.Editor
+{
+    public static class BuildScript
+    {
+        public static string overloadedDevelopmentServerURL = "";
+
+        public static void CopyAssetBundlesTo(string outputPath)
+        {
+            if (!Directory.Exists(outputPath))
+                Directory.CreateDirectory(outputPath);
+            var outputFolder = GetPlatformName();
+            var source = Path.Combine(Path.Combine(Environment.CurrentDirectory, Utility.AssetBundles), outputFolder);
+            if (!Directory.Exists(source))
+                Debug.Log("No assetBundle output folder, try to build the assetBundles first.");
+            var destination = Path.Combine(outputPath, outputFolder);
+            if (Directory.Exists(destination))
+                FileUtil.DeleteFileOrDirectory(destination);
+            FileUtil.CopyFileOrDirectory(source, destination);
+        }
+
+        public static string GetPlatformName()
+        {
+            return GetPlatformForAssetBundles(EditorUserBuildSettings.activeBuildTarget);
+        }
+
+        private static string GetPlatformForAssetBundles(BuildTarget target)
+        {
+            // ReSharper disable once SwitchStatementMissingSomeCases
+            switch (target)
+            {
+                case BuildTarget.Android:
+                    return "Android";
+                case BuildTarget.iOS:
+                    return "iOS";
+                case BuildTarget.WebGL:
+                    return "WebGL";
+                case BuildTarget.StandaloneWindows:
+                case BuildTarget.StandaloneWindows64:
+                    return "Windows";
+                case BuildTarget.StandaloneOSXIntel:
+                case BuildTarget.StandaloneOSXIntel64:
+                case BuildTarget.StandaloneOSXUniversal:
+                    return "OSX";
+                default:
+                    return null;
+            }
+        }
+
+        private static string[] GetLevelsFromBuildSettings()
+        {
+            return new[] {EditorBuildSettings.scenes[0].path};
+        }
+
+        private static string GetAssetBundleManifestFilePath()
+        {
+            var relativeAssetBundlesOutputPathForPlatform = Path.Combine(Utility.AssetBundles, GetPlatformName());
+            return Path.Combine(relativeAssetBundlesOutputPathForPlatform, GetPlatformName()) + ".manifest";
+        }
+
+        public static void BuildStandalonePlayer()
+        {
+            var outputPath = EditorUtility.SaveFolderPanel("Choose Location of the Built Game", "", "");
+            if (outputPath.Length == 0)
+                return;
+
+            var levels = GetLevelsFromBuildSettings();
+            if (levels.Length == 0)
+            {
+                Debug.Log("Nothing to build.");
+                return;
+            }
+
+            var targetName = GetBuildTargetName(EditorUserBuildSettings.activeBuildTarget);
+            if (targetName == null)
+                return;
+#if UNITY_5_4 || UNITY_5_3 || UNITY_5_2 || UNITY_5_1 || UNITY_5_0
+			BuildOptions option = EditorUserBuildSettings.development ? BuildOptions.Development : BuildOptions.None;
+			BuildPipeline.BuildPlayer(levels, outputPath + targetName, EditorUserBuildSettings.activeBuildTarget, option);
+#else
+            var buildPlayerOptions = new BuildPlayerOptions
+            {
+                scenes = levels,
+                locationPathName = outputPath + targetName,
+                assetBundleManifestPath = GetAssetBundleManifestFilePath(),
+                target = EditorUserBuildSettings.activeBuildTarget,
+                options = EditorUserBuildSettings.development ? BuildOptions.Development : BuildOptions.None
+            };
+            BuildPipeline.BuildPlayer(buildPlayerOptions);
+#endif
+        }
+
+        public static string CreateAssetBundleDirectory()
+        {
+            // Choose the output path according to the build target.
+            var outputPath = Path.Combine(Utility.AssetBundles, GetPlatformName());
+            if (!Directory.Exists(outputPath))
+                Directory.CreateDirectory(outputPath);
+
+            return outputPath;
+        }
+
+        private static Dictionary<string, string> GetVersions(AssetBundleManifest manifest)
+        {
+            var items = manifest.GetAllAssetBundles();
+            return items.ToDictionary(item => item, item => manifest.GetAssetBundleHash(item).ToString());
+        }
+
+        private static void LoadVersions(string versionsTxt, IDictionary<string, string> versions)
+        {
+            if (versions == null)
+                throw new ArgumentNullException("versions");
+            if (!File.Exists(versionsTxt))
+                return;
+            using (var s = new StreamReader(versionsTxt))
+            {
+                string line;
+                while ((line = s.ReadLine()) != null)
+                {
+                    if (line == string.Empty)
+                        continue;
+                    var fields = line.Split(':');
+                    if (fields.Length > 1)
+                        versions.Add(fields[0], fields[1]);
+                }
+            }
+        }
+
+        private static void SaveVersions(string versionsTxt, Dictionary<string, string> versions)
+        {
+            if (File.Exists(versionsTxt))
+                File.Delete(versionsTxt);
+            using (var s = new StreamWriter(versionsTxt))
+            {
+                foreach (var item in versions)
+                    s.WriteLine(item.Key + ':' + item.Value);
+                s.Flush();
+                s.Close();
+            }
+        }
+
+        public static void BuildManifest()
+        {
+            var manifest = GetManifest();
+            AssetImporter.GetAtPath(Utility.AssetsManifestAsset).assetBundleName = manifest.name;
+            AssetDatabase.RemoveUnusedAssetBundleNames();
+            var assetBundles = AssetDatabase.GetAllAssetBundleNames();
+            var list = new List<AssetData>();
+            for (int i = 0, max = assetBundles.Length; i < max; i++)
+            {
+                var item = assetBundles[i];
+                var assetPaths = AssetDatabase.GetAssetPathsFromAssetBundle(item);
+                list.AddRange(assetPaths.Select(assetPath => new AssetData {name = assetPath, bundle = i}));
+            }
+
+            manifest.bundles = assetBundles;
+            manifest.assets = list.ToArray();
+            manifest.downloadURL = GetServerURL();
+
+            EditorUtility.SetDirty(manifest);
+            AssetDatabase.SaveAssets();
+        }
+
+        public static void BuildAssetBundles()
+        {
+            BuildManifest();
+            // Choose the output path according to the build target.
+            var outputPath = CreateAssetBundleDirectory();
+
+            const BuildAssetBundleOptions options = BuildAssetBundleOptions.ChunkBasedCompression;
+
+            var manifest =
+                BuildPipeline.BuildAssetBundles(outputPath, options, EditorUserBuildSettings.activeBuildTarget);
+            var versionsTxt = outputPath + "/versions.txt";
+            var versions = new Dictionary<string, string>();
+            LoadVersions(versionsTxt, versions);
+
+            var buildVersions = GetVersions(manifest);
+
+            var updates = new List<string>();
+
+            foreach (var item in buildVersions)
+            {
+                string hash;
+                var isNew = true;
+                if (versions.TryGetValue(item.Key, out hash))
+                    if (hash.Equals(item.Value))
+                        isNew = false;
+                if (isNew)
+                    updates.Add(item.Key);
+            }
+
+            if (updates.Count > 0)
+            {
+                using (var s = new StreamWriter(File.Open(outputPath + "/updates.txt", FileMode.Append)))
+                {
+                    s.WriteLine(DateTime.Now.ToFileTime() + ":");
+                    foreach (var item in updates)
+                        s.WriteLine(item);
+                    s.Flush();
+                    s.Close();
+                }
+
+                SaveVersions(versionsTxt, buildVersions);
+            }
+            else
+            {
+                Debug.Log("nothing to update.");
+            }
+
+            string[] ignoredFiles = {GetPlatformName(), "versions.txt", "updates.txt", "manifest"};
+
+            var files = Directory.GetFiles(outputPath, "*", SearchOption.AllDirectories);
+
+            var deletes = (from t in files
+                let file = t.Replace('\\', '/').Replace(outputPath.Replace('\\', '/') + '/', "")
+                where !file.EndsWith(".manifest") && !Array.Exists(ignoredFiles, s => s.Equals(file))
+                where !buildVersions.ContainsKey(file)
+                select t).ToList();
+
+            foreach (var delete in deletes)
+            {
+                if (!File.Exists(delete))
+                    continue;
+                File.Delete(delete);
+                File.Delete(delete + ".manifest");
+            }
+
+            deletes.Clear();
+        }
+
+        private static string GetBuildTargetName(BuildTarget target)
+        {
+            var name = PlayerSettings.productName + "_" + PlayerSettings.bundleVersion;
+            // ReSharper disable once SwitchStatementMissingSomeCases
+            switch (target)
+            {
+                case BuildTarget.Android:
+                    return "/" + name + PlayerSettings.Android.bundleVersionCode + ".apk";
+
+                case BuildTarget.StandaloneWindows:
+                case BuildTarget.StandaloneWindows64:
+                    return "/" + name + PlayerSettings.Android.bundleVersionCode + ".exe";
+
+                case BuildTarget.StandaloneOSXIntel:
+                case BuildTarget.StandaloneOSXIntel64:
+                case BuildTarget.StandaloneOSXUniversal:
+                    return "/" + name + ".app";
+
+                case BuildTarget.WebGL:
+                case BuildTarget.iOS:
+                    return "";
+                // Add more build targets for your own.
+                default:
+                    Debug.Log("Target not implemented.");
+                    return null;
+            }
+        }
+
+        private static T GetAsset<T>(string path) where T : ScriptableObject
+        {
+            var asset = AssetDatabase.LoadAssetAtPath<T>(path);
+            if (asset == null)
+            {
+                asset = ScriptableObject.CreateInstance<T>();
+                AssetDatabase.CreateAsset(asset, path);
+                AssetDatabase.SaveAssets();
+            }
+
+            return asset;
+        }
+
+        public static Settings GetSettings()
+        {
+            const string path = "Assets/Settings.asset";
+            return GetAsset<Settings>(path);
+        }
+
+        public static AssetsManifest GetManifest()
+        {
+            return GetAsset<AssetsManifest>(Utility.AssetsManifestAsset);
+        }
+
+        public static string GetServerURL()
+        {
+            string downloadURL;
+            if (string.IsNullOrEmpty(overloadedDevelopmentServerURL) == false)
+            {
+                downloadURL = overloadedDevelopmentServerURL;
+            }
+            else
+            {
+                IPHostEntry host;
+                string localIP = "";
+                host = Dns.GetHostEntry(Dns.GetHostName());
+                foreach (IPAddress ip in host.AddressList)
+                {
+                    if (ip.AddressFamily == AddressFamily.InterNetwork)
+                    {
+                        localIP = ip.ToString();
+                        break;
+                    }
+                }
+
+                downloadURL = "http://" + localIP + ":7888/";
+            }
+
+            return downloadURL;
+        }
+    }
+}
